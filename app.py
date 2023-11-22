8381c1e8dd4f047c225f4390ed333d61ebaba165
nara sedong <narasedong@naraui-MacBookAir.local>
2023-11-22 22:39:14 +0900

    test

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "test"
+    result = add.delay(1, 2)
+    return result.get()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)

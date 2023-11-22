39fc80d09c66e1218f9ecc78abdbd40536d190e4
nara sedong <narasedong@naraui-MacBookAir.local>
2023-11-22 23:30:14 +0900

    test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # encoding=utf8
 from flask import Flask
 from dotenv import load_dotenv
-from task import add
+from task import send_mail
 import os
 import smtplib
 from email.mime.text import MIMEText
@@ -12,27 +12,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # result = add.delay(1, 2)
     return "main"
 
 @app.route("/mail",methods=['POST'])
-def mail():          
-    content = "test"
-    title = 'test'
-
-    sender = os.getenv("MAIL_ADDRESS")
-    receiver = os.getenv("MAIL_ADDRESS")
-    app_password = os.getenv("APP_PASSWORD")
-
-    smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login(sender, app_password)
-    msg = MIMEText(content)
-    msg['Subject'] = title
-    smtp.sendmail(sender, receiver, msg.as_string())
-    smtp.quit()
-
+def mail():
+    result = send_mail.delay()
     return "MAIL"
 
 if __name__ == '__main__':
--- task.py
+++ task.py
@@ -1,6 +1,8 @@
 from celery import Celery
 from dotenv import load_dotenv
 import os
+import smtplib
+from email.mime.text import MIMEText
 
 load_dotenv()
 
@@ -9,3 +11,23 @@ app = Celery('tasks', backend='rpc://', broker='pyamqp://'+os.getenv("RABBITMQ_U
 @app.task
 def add(x, y):
     return x + y
+
+@app.task
+def send_mail():
+    content = "test"
+    title = 'test'
+
+    sender = os.getenv("MAIL_ADDRESS")
+    receiver = os.getenv("MAIL_ADDRESS")
+    app_password = os.getenv("APP_PASSWORD")
+
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(sender, app_password)
+    msg = MIMEText(content)
+    msg['Subject'] = title
+    smtp.sendmail(sender, receiver, msg.as_string())
+    smtp.quit()
+
+    return "MAIL"

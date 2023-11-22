a405bcb1b198a5b2e10e20b7415d21bc32cc960b
nara sedong <narasedong@naraui-MacBookAir.local>
2023-11-22 23:55:50 +0900

    test

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from task import send_mail
 import os
 import smtplib
 from email.mime.text import MIMEText
+import requests
 
 load_dotenv()
 
@@ -12,7 +13,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    api_key = os.getenv("API_KEY")
+    url = "http://api.weatherapi.com/v1/current.json?key="+api_key+"&q=seoul&aqi=no"
+    response = requests.get(url).json()
+    
+    
+    return render_template('index.html', localtime = response.location.localtime)
 
 @app.route("/mail",methods=['POST'])
 def mail():
